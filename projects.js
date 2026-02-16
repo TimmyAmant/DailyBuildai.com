7799277fd3541d43647f638c946ec6ba8f32c4a1
Timothy St Amant <timmyamant@gmail.com>
2026-02-15 22:00:39 -0500

    Update projects.js to fix folder name

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ const projects = [
     id: 'day1',
     title: 'Random Quote Generator',
     description: 'A minimalist quote generator with a curated collection of inspirational quotes from around the web. Elegant typography and a developer-inspired dark theme. Built with vanilla JavaScript.',
-    url: './Day1/index.html',
+    url: 'Day1/',
     githubUrl: 'https://github.com/TimmyAmant/DailyBuildai.com/tree/main/Day1',
     screenshot: null, // Add path like 'screenshots/day1.png' when you have one
   },
